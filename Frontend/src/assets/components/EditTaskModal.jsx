5357b453260920b880e85f612cf4236b536b0411
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-06-05 12:17:06 +0200

    added. milestone 11

--- Frontend/src/assets/components/EditTaskModal.jsx
+++ Frontend/src/assets/components/EditTaskModal.jsx
@@ -1,13 +1,5 @@
 /*
-Deve accettare i seguenti props:
-show (boolean): determina se la modale è visibile.
-onClose (function): funzione per chiudere la modale.
-task (object): oggetto che rappresenta il task da modificare.
-onSave (function): funzione che viene chiamata al salvataggio con il task aggiornato.
-Utilizzare il componente Modal per creare la modale di modifica, passandogli i seguenti valori:
-title: "Modifica Task".
-content: un form contenente i campi del task da modificare.
-onConfirm: deve attivare il submit del form.
+
 */
 import Modal from "./Modal"
 import { GlobalContext } from "../../context/GlobalContext"
--- Frontend/src/assets/components/TaskList.jsx
+++ Frontend/src/assets/components/TaskList.jsx
@@ -1,11 +1,57 @@
-import { useContext, useEffect } from "react"
-import {Link} from "react-router-dom"
+/*
+Implementare la logica di ordinamento con useMemo(), in modo che l’array ordinato venga ricalcolato solo quando cambiano tasks, sortBy o sortOrder:
+Ordinamento per title → alfabetico (localeCompare).
+Ordinamento per status → ordine predefinito: "To do" < "Doing" < "Done".
+Ordinamento per createdAt → confrontando il valore numerico della data (.getTime()).
+Applicare sortOrder per definire se l’ordine è crescente o decrescente.
+*/
+
+import { useContext, useEffect, useState, useMemo } from "react"
 import { GlobalContext } from "../../context/GlobalContext"
 import TaskRow from "../TaskRow"
+import useTasks from "./useTasks"
 
 export default function TaskList() {
-
   const { tasks, setTasks, fetchTasks } = useContext(GlobalContext)
+  const [sortBy, setSortBy] = useState("createdAt")
+  const [sortOrder, setsortOrder] = useState(1)
+
+
+  const sortTask = (e) => {
+    const selectedSortBy = e.currentTarget.innerText;
+    if (sortBy.trim().toLowerCase() === selectedSortBy) {
+      setsortOrder(selectedSortBy * -1)
+    } else {
+      setSortBy(selectedSortBy)
+      setsortOrder(1)
+    }
+
+  }
+
+  const sortLogic = useMemo(() => {
+    const sorted = [...tasks];
+    sorted.sort((a, b) => {
+      if (sortBy === 'title') {
+        return a.title.localeCompare(b.title) * sortOrder;
+      }
+
+      else if (sortBy === 'status') {
+        const order = { 'To do': 0, 'Doing': 1, 'Done': 2 };
+        return (order[a.status] - order[b.status]) * sortOrder;
+
+      } else if (sortBy === "createdAt") {
+        const aTime = new Date(a.createdAt).getTime();
+        const bTime = new Date(b.createdAt).getTime();
+        return (aTime - bTime) * sortOrder;
+
+      }
+      return 0;
+
+    })
+    return sorted;
+  }, [tasks, sortBy, sortOrder])
+
+
   useEffect(() => {
     fetchTasks()
   }, [])
@@ -16,13 +62,13 @@ export default function TaskList() {
       <table className="task-table">
         <thead>
           <tr>
-            <th className="task-header">Nome</th>
-            <th className="task-header">Stato</th>
-            <th className="task-header">Data di Creazione</th>
+            <th className="task-header" onClick={sortTask}>title</th>
+            <th className="task-header" onClick={sortTask}>status</th>
+            <th className="task-header" onClick={sortTask}>createdAt</th>
           </tr>
         </thead>
         <tbody>
-          {tasks.map(task => (
+          {sortLogic.map(task => (
             <TaskRow
               key={task.id}
               title={task.title}
